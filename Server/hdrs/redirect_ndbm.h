9d5b19ae03bc987dec0391a5d2afd53ca7dd6068
Ambrosia <chaosstar@gmail.com>
2014-08-17 11:16:31 +0000

    Added missing #ifndefe QDBM define in redirect_ndbm.h

--- Server/hdrs/redirect_ndbm.h
+++ Server/hdrs/redirect_ndbm.h
@@ -1,6 +1,9 @@
 #ifdef BROKEN_NDBM
-#include "../src/gdbm-1.8.3/ndbm.h"
-// #include "../src/qdbm/relic.h"
+  #ifndef QDBM
+    #include "../src/gdbm-1.8.3/ndbm.h"
+  #else
+    #include "../src/qdbm/relic.h"
+  #endif
 #else
-#include <ndbm.h>
+  #include <ndbm.h>
 #endif
