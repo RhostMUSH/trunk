3dee9bd87f0e1c76d373141772817a10be38a40e
Ambrosia <chaosstar@gmail.com>
2014-09-03 18:48:42 +0000

    Reverted to nicer version of large LBUF handling in atr_get_raw. This seems to work fine now.

--- Server/src/db.c
+++ Server/src/db.c
@@ -2376,13 +2376,13 @@ atr_get_raw(dbref thing, int atr)
     {
       if(strlen(a) > (LBUF_SIZE-1))
       {
-         //*(a+LBUF_SIZE-2)='\0';
-         Attr *safebuff=malloc(LBUF_SIZE); 
-         strncpy(safebuff,a,LBUF_SIZE-1);
-         *(safebuff+LBUF_SIZE-1)='\0';
-         free(a);
-         a=NULL;
-         return safebuff;
+         *(a+LBUF_SIZE-2)='\0';
+         //Attr *safebuff=malloc(LBUF_SIZE); 
+         //strncpy(safebuff,a,LBUF_SIZE-1);
+         //*(safebuff+LBUF_SIZE-1)='\0';
+         //free(a);
+         //a=NULL;
+         //return safebuff;
       }
     }
     return a;
