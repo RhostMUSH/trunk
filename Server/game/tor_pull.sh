5e617a00f7b054836477ae30a655bb137853c07e
Chalice <chaosstar@gmail.com>
2014-10-12 21:10:02 +0200

    Another revision for the TOR script. Including handling of Ashen's hidemyass static proxy list.

--- Server/game/tor_pull.sh
+++ Server/game/tor_pull.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 #
-rm blacklist.txt
-if [ -f Tor_ip_list_EXIT.csv ]
-then
-   cat /dev/null > blacklist.tmp
-   touch blacklist.txt
-   while read line
-   do
-      xx=`grep -c ${line} Tor_ip_list_EXIT.csv`
-      if [ ${xx} -eq 0 ]
-      then
-         echo "${line}" >> blacklist.tmp
-      fi
-   done < blacklist.txt
-   rm -f Tor_ip_list_EXIT.csv
-else
-   cp -f blacklist.txt blacklist.tmp
-fi
 wget http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv
 if [ $? -ne 0 ]
 then
    wget http://torstatus.rueckgr.at/ip_list_exit.php/Tor_ip_list_EXIT.csv
 fi
+if [ -f Tor_ip_list_EXIT.csv ]
+then
+  rm -f blacklist.txt
+  cat Tor_ip_list_EXIT.csv > blacklist.txt
+fi
 
 # Additional TorProject list. Needs you to specify your IP before uncommenting!
 # Checks which nodes can reach your IP. Accepts an optional &port=<port> param.
+
 #wget -q -O - "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=YOUR.IP.ADDRESS.HERE" -U NoSuchBrowser/1.0 > bulk.txt
-#cat bulk.txt |fgrep -v '#' > bulk2.txt
+if [ -f bulk.txt ]
+then
+    cat bulk.txt |fgrep -v '#' > bulk2.txt
+    if [ -f Tor_ip_list_EXIT.csv ]
+    then
+      cat bulk2.txt >> blacklist.txt
+    else
+      rm -f blacklist.txt
+      cat bulk2.txt 2>/dev/null|sort -u > blacklist.txt
+    fi
+fi
 
-# combine them
-cat blacklist.tmp blacklist.tmp2 Tor_ip_list_EXIT.csv bulk2.txt 2>/dev/null|sort -u > blacklist.txt
+cat hidemyass_static.txt > blacklist.txt
 
-rm -f blacklist.tmp blacklist.tmp2 proxy.txt Tor_ip_list_EXIT.csv bulk.txt bulk2.txt
+cat blacklist.txt | sort -u > blacklist.tmp
+cat blacklist.tmp > blacklist.txt
+
+rm -f Tor_ip_list_EXIT.csv bulk.txt bulk2.txt blacklist.tmp
