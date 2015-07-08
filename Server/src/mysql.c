cd49c213c3d622133463f042e3b8d213684c4dd1
Jace Hoppel <mrsenile@gmail.com>
2015-07-07 20:48:58 -0500

    Test for a possible fix on keeping mysql connected more

--- Server/src/mysql.c
+++ Server/src/mysql.c
@@ -310,6 +310,9 @@ static int sql_query(dbref player,
    * generating a #-1. Notify the player, too, and set the return code.
    */
   
+  if ( mysql_struct && !mysql_ping(mysql_struct) ) {
+     sql_shutdown(player);
+  }
   if (!mysql_struct) {
     /* Try to reconnect. */
     retries = 0;
