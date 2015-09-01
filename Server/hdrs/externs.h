66dc09bef6e656cf5fefd014dc20ae48fde66406
Jace Hoppel <mrsenile@gmail.com>
2015-09-01 01:43:20 -0500

    Fix for 'cd' on connect screen with permission issues

--- Server/hdrs/externs.h
+++ Server/hdrs/externs.h
@@ -880,6 +880,7 @@ extern int      FDECL(mush_crypt_validate, (dbref, const char *, const char *, i
 #define SET_RSET	4	/* set() is really rset() */
 #define SET_TREE	8	/* set() the entire trees */
 #define SET_TREECHK	16	/* Verify we can set trees */
+#define SET_MUFFLE	32	/* Totally  muffle set messages */
 #define	SHUTDN_NORMAL	0	/* Normal shutdown */
 #define	SHUTDN_PANIC	1	/* Write a panic dump file */
 #define	SHUTDN_EXIT	2	/* Exit from shutdown code */
