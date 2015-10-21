abd679eb8875846cc07dec75d82808e3222ba0f5
Jace Hoppel <mrsenile@gmail.com>
2015-10-20 23:32:34 -0500

    Stack limit readme and protection on function_recursion_limit

--- Server/src/conf.c
+++ Server/src/conf.c
@@ -16,6 +16,7 @@
 #include "alloc.h"
 #include "flags.h"
 #include "udb_defs.h"
+#include "debug.h"
 
 /* ---------------------------------------------------------------------------
  * CONFPARM: Data used to find fields in CONFDATA.
@@ -1034,6 +1035,23 @@ CF_HAND(cf_vint)
     if ((vp_old < 0) || (vp_old > i_ceil)) {
         if ( !mudstate.initializing) {
            notify(player, unsafe_tprintf("%s Value must be between 0 and %d.", s_buf, i_ceil));
+        }
+	return -1;
+    } else {
+        *vp = vp_old;
+	return 0;
+    }
+}
+
+CF_HAND(cf_recurseint)
+{
+    int i_ceil = 50, vp_old=0;
+  
+    sscanf(str, "%d", &vp_old);
+    i_ceil = STACKMAX / 10;
+    if ((vp_old < 0) || (vp_old > i_ceil)) {
+        if ( !mudstate.initializing) {
+           notify(player, unsafe_tprintf("Value must be between 0 and %d [Stackmax is %d].", i_ceil, STACKMAX));
         }
 	return -1;
     } else {
@@ -3748,7 +3766,7 @@ CONF conftable[] =
      (char *) "The current function invocation limit.\r\n"\
               "                             Default: 2500   Value: %d"},
     {(char *) "function_recursion_limit",
-     cf_int, CA_GOD | CA_IMMORTAL, &mudconf.func_nest_lim, 0, 0, CA_WIZARD,
+     cf_recurseint, CA_GOD | CA_IMMORTAL, &mudconf.func_nest_lim, 0, 0, CA_WIZARD,
      (char *) "The current function recursion limit.\r\n"\
               "(Max is 300)                 Default: 50   Value: %d"},
     {(char *) "wildmatch_limit",
@@ -5192,7 +5210,8 @@ void list_options_values(dbref player, int p_val, char *s_val)
       if (((tp->interpreter == cf_int) ||
            (tp->interpreter == cf_int_runtime) ||
            (tp->interpreter == cf_mailint) ||
-           (tp->interpreter == cf_vint)) &&
+           (tp->interpreter == cf_vint) ||
+           (tp->interpreter == cf_recurseint)) &&
           (check_access(player, tp->flags2, 0, 0))) {
       cntr++;
       }
@@ -5209,7 +5228,8 @@ void list_options_values(dbref player, int p_val, char *s_val)
       if (((tp->interpreter == cf_int) ||
            (tp->interpreter == cf_int_runtime) ||
            (tp->interpreter == cf_mailint) ||
-           (tp->interpreter == cf_vint)) &&
+           (tp->interpreter == cf_vint) ||
+           (tp->interpreter == cf_recurseint)) &&
           (check_access(player, tp->flags2, 0, 0))) {
          if ( s_val && *s_val ) {
             if ( !quick_wild(s_val, tp->pname) ) {
@@ -5283,6 +5303,7 @@ void cf_display(dbref player, char *param_name, int key, char *buff, char **bufc
                     (tp->interpreter == cf_int_runtime) ||
                     (tp->interpreter == cf_mailint) ||
                     (tp->interpreter == cf_vint) ||
+                    (tp->interpreter == cf_recurseint) ||
 		    (tp->interpreter == cf_sidefx && !bVerboseSideFx)) {
 
                    sprintf(tempbuff, "%d", *(tp->loc));
