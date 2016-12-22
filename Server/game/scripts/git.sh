4c219c41cee226f631152850430cfdc2f0d28070
Jace Hoppel <mrsenile@gmail.com>
2016-12-22 01:49:59 -0600

    cleanup of git.sh

--- Server/game/scripts/git.sh
+++ Server/game/scripts/git.sh
@@ -2,10 +2,22 @@
 x="$@"
 if [ -z "$x" ]
 then
-   git rev-parse HEAD
+   git rev-parse HEAD 2>/dev/null
+   if [ $? -ne 0 ]
+   then
+      echo "Not a git repo."
+   fi
 elif [ "$x" = "short" ]
 then
    git rev-parse --short HEAD
+   if [ $? -ne 0 ]
+   then
+      echo "Not a git repo."
+   fi
 else
    git rev-parse HEAD
+   if [ $? -ne 0 ]
+   then
+      echo "Not a git repo."
+   fi
 fi
