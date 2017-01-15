3e3793ba6c4725857de1a2196675655e940bad2f
Ambrosia <chaosstar@gmail.com>
2017-01-15 10:01:40 +0100

    Release change to Rhost v4.0.0p0

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -4,7 +4,7 @@
 
 #include "copyright.h"
 
-#define MUSH_VERSION            "3.9.5"         /* Base version number*/
+#define MUSH_VERSION            "4.0.0"         /* Base version number*/
 
 #if defined(ZENTY_ANSI) && defined(REALITY_LEVELS)
 #define EXT_MUSH_VER "RL(A)"
@@ -16,9 +16,9 @@
 #define EXT_MUSH_VER ""
 #endif
 
-#define PATCHLEVEL		4		/* Patch sequence number     */
+#define PATCHLEVEL		0		/* Patch sequence number     */
 #define PATCHLEVELEXT		""
-#define	MUSH_RELEASE_DATE	"11/23/2016"	/* Source release date       */
+#define	MUSH_RELEASE_DATE	"01/15/2017"	/* Source release date       */
 
 /* Define if an ALPHA release */
 #define ALPHA 1   		
