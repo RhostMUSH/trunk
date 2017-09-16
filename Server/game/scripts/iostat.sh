71c64b789d14809d16566cbbc6af64e2ad0e6564
Jace Hoppel <mrsenile@gmail.com>
2017-09-16 14:28:55 -0500

    Update to iostat.sh

--- Server/game/scripts/iostat.sh
+++ Server/game/scripts/iostat.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-ps u -p $(cat ./netrhost.pid)
+ps u -p $(cat ./netrhost.pid 2>/dev/null) 2>/dev/null
 echo ""
 echo $(uptime 2>/dev/null)
-iostat -y > /dev/null 2>&1
+iostat > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-   iostat -xd 1 1|grep  "^Device" && iostat -xd -y 1 3|egrep -v "(^Device|^$)"|awk '{if(NR>1)print}'|sort
-else
-   iostat -xd 1 1|grep  "^Device" && iostat -xd 1 3|egrep -v "(^Device|^$)"|awk '{if(NR>1)print}'|sort
+   iostat -y > /dev/null 2>&1
+   if [ $? -eq 0 ]
+   then
+      iostat -xd 1 1|grep  "^Device" && iostat -xd -y 1 3|egrep -v "(^Device|^$)"|awk '{if(NR>1)print}'|sort
+   else
+      iostat -xd 1 1|grep  "^Device" && iostat -xd 1 3|egrep -v "(^Device|^$)"|awk '{if(NR>1)print}'|sort
+   fi
 fi
