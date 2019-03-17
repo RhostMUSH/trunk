1accc42a126b0355e58d46d78f0d19f9af66dde7
Jace Hoppel <mrsenile@gmail.com>
2019-03-17 00:52:45 -0500

    Modified the math execscript library to have user-supplied precision

--- Server/game/scripts/math.sh
+++ Server/game/scripts/math.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-export BC_LINE_LENGTH=3998
+export BC_LINE_LENGTH=32000
 bc --version > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-   echo "#-1 I AM NOT A MATH-HEAD"
+   echo "#-1 I AM NOT A MATH-HEAD (no supporting library)"
 else
-   echo "$@"|bc 2>&1
+   gl_arg=$(echo "$@"|cut -f1 -d"|")
+   gl_scale=$(echo "$@"|cut -f2 -d"|")
+   gl_scale=$(expr ${gl_scale} + 0 2>/dev/null)
+   if [ -z "${gl_scale}" ]
+   then
+      gl_scale=20000
+   fi
+   echo "scale=${gl_scale};${gl_arg}"|bc 2>&1
 fi
