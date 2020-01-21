2e14d3bb4057b4f787be48e4d8a7f52ebf7217b6
Jace Hoppel <mrsenile@gmail.com>
2020-01-21 13:20:46 -0600

    Added quickspell to Rhost

---
+++ Server/game/scripts/qspell.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+aspell --version > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+   echo "#-1 Speller is not installed."
+   exit 0
+else
+   lc_words=$(echo "$@" | aspell -a | egrep -v "(^\*|^\@|^$)"|cut -d ' ' -f 2)
+   for i in $@
+   do
+      if [ $(echo "${lc_words}"|grep -wc "$i") -gt 0 ]
+      then
+         lc_i="%ch%cr${i}%cn"
+      else
+         lc_i="${i}"
+      fi
+      if [ -z "$list" ]
+      then
+         list="${lc_i}"
+      else
+         list="${list} ${lc_i}"
+      fi
+   done
+   echo -n "${list}"
+fi
