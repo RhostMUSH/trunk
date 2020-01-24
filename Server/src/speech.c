1b6188ab4b61361a63deb9f442a3330928702b9f
Ambrosia <chaosstar@gmail.com>
2020-01-24 12:31:30 +0100

    Fixed some GCC warnings in pcre.c and speech.c

--- Server/src/speech.c
+++ Server/src/speech.c
@@ -3393,7 +3393,7 @@ int getword(char*,char*);
 
 void do_channel(dbref player, dbref cause, int key, char *arg1)
 {
-  char buf[LBUF_SIZE], buff2[LBUF_SIZE];
+  char buf[LBUF_SIZE], buf2[LBUF_SIZE], buff2[LBUF_SIZE];
   int aflags;
   dbref aowner;
   char *tmp, *tpr_buff, *tprp_buff, *tstrtokr;
@@ -3419,6 +3419,8 @@ void do_channel(dbref player, dbref cause, int key, char *arg1)
       tmp_word = strtok_r( tmp, " " , &tstrtokr);
       tprp_buff = tpr_buff = alloc_lbuf("do_channel");
       while( tmp_word ) {
+  buf2[0] = '\0';
+
          if ( iny == 0 ) {
             iny = 1;
             strcpy( buf, tmp_word );
@@ -3430,11 +3432,13 @@ void do_channel(dbref player, dbref cause, int key, char *arg1)
             inx = 0;
          }
          else if ( inx < 1 ) {
-            sprintf( buf, "%-20.20s     %-20.20s", buf, tmp_word );
+            sprintf( buf2, "%-20.20s     %-20.20s", buf, tmp_word );
+            sprintf(buf, buf2);
             inx++;
          }
          else {
-            sprintf( buf, "%-45.45s     %-20.20s", buf, tmp_word );
+            sprintf( buf2, "%-45.45s     %-20.20s", buf, tmp_word );
+            sprintf(buf, buf2);
             inx++;
          }
       tmp_word = strtok_r( NULL, " ", &tstrtokr );
