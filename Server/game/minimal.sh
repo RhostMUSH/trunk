87e133932d85a49ea2eae729d3aad03b710336ab
Jace Hoppel <mrsenile@gmail.com>
2020-02-04 02:48:00 -0600

    Modification of minimal db to handle running games

--- Server/game/minimal.sh
+++ Server/game/minimal.sh
@@ -7,6 +7,40 @@ then
    echo "Aborted by user."
    exit 1
 fi
+
+if [ -f "netrhost.pid" ]
+then
+   ps -p `cat netrhost.pid`> /dev/null 2>&1
+   if [ $? -eq 0 ]
+   then
+      echo "You have a mush running.  You must shut it down to copy in the new database."
+      echo "Shut it down? (Y/N): "|tr -d '\012'
+      read ans
+      if [ "$ans" == "Y" -o "$ans" == "y" ]
+      then
+         kill -USR2 `cat netrhost.pid`
+         sleep 5
+         ps -p `cat netrhost.pid`> /dev/null 2>&1
+         if [ $? -eq 0 ]
+         then
+            echo "Did not shutdown cleanly.  Attempting TERM."
+            kill -TERM `cat netrhost.pid`
+            sleep 5
+            ps -p `cat netrhost.pid`> /dev/null 2>&1
+            if [ $? -eq 0 ]
+            then
+               echo "Process was not killed with TERM.  Please shutdown your mush then rerun script."
+               exit 1
+            fi
+         fi
+      else
+         echo "Aborted by user."
+         exit 1
+      fi
+      echo "Mush has been shutdown."
+   fi
+fi
+   
 ls ./data/netrhost.gdbm* > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
