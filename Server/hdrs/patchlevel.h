ec5e26203c843519e0bcc7455c4b9c45a94b9f2f
Jace Hoppel <mrsenile@gmail.com>
2020-02-23 02:48:55 -0600

    Forgot to up the version number

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -16,9 +16,9 @@
 #define EXT_MUSH_VER ""
 #endif
 
-#define PATCHLEVEL		0		/* Patch sequence number     */
+#define PATCHLEVEL		1		/* Patch sequence number     */
 #define PATCHLEVELEXT		""
-#define	MUSH_RELEASE_DATE	"12/15/2019"	/* Source release date       */
+#define	MUSH_RELEASE_DATE	"02/23/2020"	/* Source release date       */
 
 /* Define if an ALPHA release */
 #define ALPHA 1   		
