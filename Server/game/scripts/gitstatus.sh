b894572e90585c9366805d28b0be4706cba780a1
warmstarter <79870389+warmstarter@users.noreply.github.com>
2021-04-10 13:38:27 -0400

    gitstatus.sh execscript for checking for updates and (#81)
    via mushcode/mushcron (softcode not included in commit)

---
+++ Server/game/scripts/gitstatus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Checks the status of your local repo versus the upstream repo.
+# 0: diverged
+# 1: up-to-date
+# 2: behind
+# 3: ahead
+
+a="master" b="origin/master"
+base=$( git merge-base $a $b )
+aref=$( git rev-parse  $a )
+bref=$( git rev-parse  $b )
+
+if [[ $aref == "$bref" ]]; then
+  echo 1
+elif [[ $aref == "$base" ]]; then
+  echo 2
+elif [[ $bref == "$base" ]]; then
+  echo 3
+else
+  echo 0
+fi
