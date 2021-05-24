e7e4d2ec5297f839d3beab04c0482247de3785ae
warmstarter <79870389+warmstarter@users.noreply.github.com>
2021-05-24 12:49:56 +0200

    Small version fix on RockJobs

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -4,7 +4,7 @@
 
 #include "copyright.h"
 
-#define MUSH_VERSION            "4.2.0-40"         /* Base version number*/
+#define MUSH_VERSION            "4.2.0-41"         /* Base version number*/
 
 #if defined(ZENTY_ANSI) && defined(REALITY_LEVELS)
 #define EXT_MUSH_VER "RL(A)"
