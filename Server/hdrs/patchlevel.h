66ef98b2d381d9c386eef0ee38d965d6f0bedb29
Ambrosia <chaosstar@gmail.com>
2021-06-09 14:57:15 +0200

    Added 'pagelock_notify' to alternate_netrhost.conf.default

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -4,7 +4,7 @@
 
 #include "copyright.h"
 
-#define MUSH_VERSION            "4.2.0-72"         /* Base version number*/
+#define MUSH_VERSION            "4.2.0-74"         /* Base version number*/
 
 #if defined(ZENTY_ANSI) && defined(REALITY_LEVELS)
 #define EXT_MUSH_VER "RL(A)"
@@ -18,7 +18,7 @@
 
 #define PATCHLEVEL		0		/* Patch sequence number     */
 #define PATCHLEVELEXT		""
-#define	MUSH_RELEASE_DATE	"06/06/2021"	/* Source release date       */
+#define	MUSH_RELEASE_DATE	"06/09/2021"	/* Source release date       */
 
 /* Define if an ALPHA release */
 /* #define ALPHA 0 */
