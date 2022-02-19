b6ae2bd4639cd51482166a8bbc40e49c73989725
Jace Hoppel <mrsenile@gmail.com>
2022-02-19 14:06:31 -0600

    fix for mail/reply & mail/forward with leading spaces

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -4,7 +4,7 @@
 
 #include "copyright.h"
 
-#define MUSH_VERSION            "4.2.0-132"         /* Base version number*/
+#define MUSH_VERSION            "4.2.0-133"         /* Base version number*/
 
 #if defined(ZENTY_ANSI) && defined(REALITY_LEVELS)
 #define EXT_MUSH_VER "RL(A)"
@@ -18,7 +18,7 @@
 
 #define PATCHLEVEL		0		/* Patch sequence number     */
 #define PATCHLEVELEXT		""
-#define	MUSH_RELEASE_DATE	"02/14/2022"	/* Source release date       */
+#define	MUSH_RELEASE_DATE	"02/19/2022"	/* Source release date       */
 
 /* Define if an ALPHA release */
 /* #define ALPHA 0 */
