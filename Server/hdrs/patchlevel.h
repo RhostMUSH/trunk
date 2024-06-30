51118fee885e9c897b8e7f881f9b1c7cc2f1994c
Ambrosia <chaosstar@gmail.com>
2024-06-30 17:49:53 +0200

    4.3.0 release - Semantic Versioning

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -43,6 +43,6 @@
 #define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "06/26/2024"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2024-06-30"          /* Source release date */
 
 #endif
