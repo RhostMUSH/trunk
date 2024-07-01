971bb6cf603afc34482ad942dbe0861fd5e252b8
Jace Hoppel <mrsenile@gmail.com>
2024-07-01 01:17:42 -0500

    Updated version

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "4" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "3" /* Normal new features and changes       */
-#define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "1" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2024-06-30"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2024-07-01"          /* Source release date */
 
 #endif
