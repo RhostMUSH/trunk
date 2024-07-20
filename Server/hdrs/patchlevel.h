da03a416a376bfc7181205dc6df71e033c89c2fd
Jace Hoppel <mrsenile@gmail.com>
2024-07-19 23:30:29 -0500

    fix for connect_methods having reversed logic

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "4" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "5" /* Normal new features and changes       */
-#define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "1" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2024-07-16"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2024-07-19"          /* Source release date */
 
 #endif
