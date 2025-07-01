521206c843317cdfabb41a24874da1f9910cdef8
Ambrosia <chaosstar@gmail.com>
2025-07-01 17:20:22 +0200

    Fixed a bug in time_paddzero displaying '2d'. Thanks Hope!

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "4" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "20" /* Normal new features and changes       */
-#define PATCH_VERSION           "1" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "2" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2025-06-05"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2025-07-01"          /* Source release date */
 
 #endif
