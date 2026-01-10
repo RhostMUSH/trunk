c808cb1fc4227f935b5891f582a39ba244cb5aa2
Jace Hoppel <mrsenile@gmail.com>
2026-01-09 20:22:49 -0600

    HOT-Fix for OpenSSL >= 3.0 for digest handling

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "4" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "27" /* Normal new features and changes       */
-#define PATCH_VERSION           "2" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "3" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2025-12-09"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-01-09"          /* Source release date */
 
 #endif
