ed33bef5867234fc1bb571f52dbfe80575b2900f
Ambrosia <chaosstar@gmail.com>
2026-06-08 12:06:52 +0200

    5.2.0 - added @list utf8 to show the UTF8 -> ASCII fallback table that gets used if someone has no UTF8 toggle set. Issue #136, originally reported by Warmstarter.

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -39,10 +39,10 @@
  */
 
 #define MAJOR_VERSION           "5" /* Major rewrites, DB system changes..   */
-#define MINOR_VERSION           "1" /* Normal new features and changes       */
-#define PATCH_VERSION           "16" /* Bugfixes, crashfixes etc.             */
+#define MINOR_VERSION           "2" /* Normal new features and changes       */
+#define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2026-06-07"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-06-08"          /* Source release date */
 
 #endif
