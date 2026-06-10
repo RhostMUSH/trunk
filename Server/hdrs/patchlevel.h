e46e91d06f97569a27615d53113209410411d289
Ambrosia <chaosstar@gmail.com>
2026-06-10 12:49:41 +0200

    5.3.0 - added support for playernames with spaces for mail sending. Either separate entries by comma, OR use quotation marks around names with spaces.

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -39,10 +39,10 @@
  */
 
 #define MAJOR_VERSION           "5" /* Major rewrites, DB system changes..   */
-#define MINOR_VERSION           "2" /* Normal new features and changes       */
-#define PATCH_VERSION           "1" /* Bugfixes, crashfixes etc.             */
+#define MINOR_VERSION           "3" /* Normal new features and changes       */
+#define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2026-06-08"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-06-10"          /* Source release date */
 
 #endif
