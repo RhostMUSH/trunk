9a5b8bf5705463defd16a07579b03e9c1baa0ec0
Ambrosia <chaosstar@gmail.com>
2026-07-23 07:46:58 +0200

    5.8.4 - Fixed a couple missing free's in the sqlite handler.

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "5" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "8" /* Normal new features and changes       */
-#define PATCH_VERSION           "3" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "4" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2026-07-18"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-07-23"          /* Source release date */
 
 #endif
