3986d5682e5c1f015d49bc80680a0674e8a2925b
Ambrosia <chaosstar@gmail.com>
2026-08-19 09:51:16 +0200

    5.9.2 - Crashfix in @tag/list related to the ansisplitter

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -40,9 +40,9 @@
 
 #define MAJOR_VERSION           "5" /* Major rewrites, DB system changes..   */
 #define MINOR_VERSION           "9" /* Normal new features and changes       */
-#define PATCH_VERSION           "1" /* Bugfixes, crashfixes etc.             */
+#define PATCH_VERSION           "2" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2026-08-02"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-08-19"          /* Source release date */
 
 #endif
