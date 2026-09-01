5863b6b2541c5be09c1deaf48c938cc6275eb519
Ambrosia <chaosstar@gmail.com>
2026-09-01 19:19:56 +0200

    5.11.0 - Bugfix: Proper color display/downsampling for .txt files and in file_object based on truecolor flag or libtelnet client capabilities. Several internal small speedups in multiple places.

--- Server/hdrs/patchlevel.h
+++ Server/hdrs/patchlevel.h
@@ -39,10 +39,10 @@
  */
 
 #define MAJOR_VERSION           "5" /* Major rewrites, DB system changes..   */
-#define MINOR_VERSION           "10" /* Normal new features and changes       */
+#define MINOR_VERSION           "11" /* Normal new features and changes       */
 #define PATCH_VERSION           "0" /* Bugfixes, crashfixes etc.             */
 #define VERSION_EXT             ""  /* "" OR "ALPHA", "BETA, "RC1", "RC2"... */
 
-#define MUSH_RELEASE_DATE       "2026-08-29"          /* Source release date */
+#define MUSH_RELEASE_DATE       "2026-09-01"          /* Source release date */
 
 #endif
